cbe3f1b7852f4558c126763ed6110d00a9c3f160
toastloop <69254179+toastloop@users.noreply.github.com>
2025-05-14 10:38:09 -0600

    Updated si_test.sh

--- si_test.sh
+++ si_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 add_si() {
-    num1=$(printf "%s\n" "$1" | numfmt --from=iec)
-    num2=$(printf "%s\n" "$2" | numfmt --from=iec)
+    num1=$(printf "%s\n" "$1" | numfmt --from=iec-i)
+    num2=$(printf "%s\n" "$2" | numfmt --from=iec-i)
     sum=$((num1 + num2))
-    printf "%s\n" "$sum" | numfmt --to=iec
+    printf "%s\n" "$sum" | numfmt --to=iec-i
 }
 add_si 1GiB 2MiB
