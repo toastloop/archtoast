4dec9970d541ee3d4d8082ef77bd188dbedc5faa
toastloop <69254179+toastloop@users.noreply.github.com>
2025-05-14 05:32:34 -0600

    Remove the README. entries from the list of fonts in 3.fonts.sh

--- 3.fonts.sh
+++ 3.fonts.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-fonts=`ls /usr/share/kbd/consolefonts | grep -v '\*' | awk '{print $1}'`
+fonts=`ls /usr/share/kbd/consolefonts | grep -v '\*' | grep -v 'README.*' | awk '{print $1}'`
 
 while true; do
 
-    echo "Select a font (default: Lat2-Terminus16, show list: list):"
+    echo "Select a font (default: Lat2-Terminus16.psfu.gz, show list: list):"
     read -r font
 
     font=`echo "$font" | tr -d '[:space:]'`
@@ -12,7 +12,7 @@ while true; do
     if [ "$font" = "list" ]; then
         echo "$fonts" | less
     elif [ -z "$font" ]; then
-        font="Lat2-Terminus16"
+        font="Lat2-Terminus16.psfu.gz"
         break
     elif echo "$fonts" | grep -q "^$font$"; then
         break
